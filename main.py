a289a19084886b0ef76757446393587c648cefc4
Marco Resendiz <113551818+maresendiz2@users.noreply.github.com>
2022-09-16 19:18:00 +0000

    learned pop

--- main.py
+++ main.py
@@ -4,20 +4,32 @@
 
 ##############################################################################################################
 # basic review
-# Find and display on the screen which character occupies the fifth position within the following word:
-"computer"
-
+# Find and display on the screen which character occupies the fifth position 
+#within the following word:
+# computer = "computer"
+# print(computer[-1])
 # Find and display the index of the last occurrence of the word "practice" in the following sentence:
-"In theory, theory and practice are the same. In practice, they are not."
+# newSentence = "In theory, theory and practice are the same. In practice, they are not."
+# print(newSentence.rindex("practice"))
 
 ###############################################Review############################################################
 #####################################challenge 1################################################################
 # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
-
+# my_list = ["apple", 3, "pear", "bus", 123]
+# print(my_list)
 # Add the element "motorcycle" to the following list of means of transportation:
 
 transportation_means = ["plane", "car", "ship", "bicycle"]
 
+transportation_means.append("motorcycle")
+#append adds an element to the end
+print(transportation_means)
+
+transportation_means.remove("car")
+print(transportation_means)
+newTrans = transportation_means.pop()
+#pop removes the last element
+print("My new trans is " +newTrans)
 # You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
 
 # here are two lists:
